a63cad40cc7bdd91e6b3b2c6897e03ff9afa26fd
Nathalie Nassar <nathalie.nassar39@gmail.com>
2022-11-25 20:30:41 +0200

    get the uploaded file in signup

--- client-app/src/view/SignupPanel.java
+++ client-app/src/view/SignupPanel.java
@@ -6,6 +6,8 @@ import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javax.swing.GroupLayout;
@@ -134,7 +136,23 @@ public class SignupPanel extends JPanel {
 					int save = file_upload.showSaveDialog(null);
 					
 					if (save == JFileChooser.APPROVE_OPTION) {
-						File file_path = new File(file_upload.getSelectedFile().getAbsolutePath());
+						//Creating a File object
+					      File file = new File(file_upload.getSelectedFile().getAbsolutePath());
+					      //Creating a FileInputStream object
+					      FileInputStream inputStream;
+						try {
+							inputStream = new FileInputStream(file);
+							  //Creating a byte array
+						      byte bytes[] = new byte[(int) file.length()];
+						      //Reading data into the byte array
+						      int numOfBytes = inputStream.read(bytes);
+						      System.out.println("Successful upload.");
+							
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					    
 					}
 				}
 			});
